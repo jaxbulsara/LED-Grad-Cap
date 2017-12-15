df43705b772a2c907733ffa7d1e67d61889e79a1
Jay Bulsara <jaxbulsara@gmail.com>
2017-12-14 19:34:49 -0500

    arduino-added serial parsing function
    android-extraneous studio files

--- led_grad_cap/led_grad_cap.ino
+++ led_grad_cap/led_grad_cap.ino
@@ -19,19 +19,27 @@ SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeig
 SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
 SMARTMATRIX_ALLOCATE_INDEXED_LAYER(indexedLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kIndexedLayerOptions);
 
-const int defaultBrightness = 100;    // 100 - full brightness
-defaultBrightness = defaultBrightness * (255 / 100);
+const int defaultBrightness = 100 * (255 / 100);    // 100 - full brightness
 
 const int defaultScrollOffset = 6;
 const rgb24 defaultBackgroundColor = {0, 0, 0}; // black
 
+// counting variable
+int i;
+
 // Teensy 3.x has the LED on pin 13
 const int ledPin = 13;
 
+// current time variable
+int currentMillis;
 
-/* -------------- */
-/* setup function */
-/* -------------- */
+// serial message variables
+String message [] = {"", ""};
+
+// initialize functions
+void log_pc(), log_bt(), serial_parse();
+
+// setup function - runs once
 void setup() {
 	// initialize led on Teensy
 	pinMode(ledPin, OUTPUT);
@@ -57,4 +65,45 @@ void setup() {
 	// clear screen
     backgroundLayer.fillScreen(defaultBackgroundColor);
     backgroundLayer.swapBuffers();
+}
+
+// loop function - runs continuously
+void loop() {
+	currentMillis = millis();
+
+	// check if bluetooth serial is sending anything
+	// convert message to lowercase for parsing
+	if(Serial1.available() or Serial.available()) {
+		while (Serial.available()) {
+			message[0] += char(Serial.read());
+		}
+
+		while (Serial1.available()) {
+			message[1] += char(Serial1.read());
+		}
+	}
+
+	// parse serial messages and then clear message variable
+	serial_parse();
+}
+
+void log_pc() {
+	Serial.println(message[0]);
+}
+
+void log_bt() {
+	Serial1.println(message[1]);
+}
+
+void serial_parse() {
+	for (i = 0; i < 1; i++) {
+		if (message[i] != "") {
+			// set to lowercase
+			message[i].toLowerCase();
+
+			Serial.println(message[i]);
+
+			message[i] = "";
+		}
+	}
 }
